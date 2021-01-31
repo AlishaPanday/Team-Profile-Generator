183af94ac1ca44527b5fcd9e8ec8cbd3fb2d5089
Alisha Panday <panday.alisha90@gmail.com>
2021-01-31 19:02:10 +0800

    function to write to html page and copy css file

--- generate-html.js
+++ generate-html.js
@@ -1,1 +1,25 @@
 //function to write to html file
+const fs = require('fs');
+
+//write html file
+
+function writePage (html) {
+    fs.writeFile('./dist/index.html', html, err =>{
+        if(err){
+            throw err
+        }
+        console.log("index.html Page generated")
+        copyCss()
+    })
+}
+
+const copyCSS = () =>{
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err){
+            throw err
+        }
+        console.log('style.css file copied')
+    });
+}
+
+module.export = writePage, copyCSS
